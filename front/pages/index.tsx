ee2a5b8ecb055b9826e7ba8379bf81126ccc9a6f
JinKris <vain65@naver.com>
2022-06-07 18:13:59 +0900

    [feat] landing ing ing

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,14 +1,12 @@
 import type { NextPage } from 'next';
 import Header from '../componnents/header/header';
-import LandingPart from '../componnents/landing/landingPart';
+import LandingPage from '../componnents/landing/landingPage';
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
-      <LandingPart text="등산메이트" />
-      <LandingPart text="사진을 찍어서.." />
-      <LandingPart text="나만의 도감" />
+      <LandingPage />
     </>
   );
 };
