8660631b2d0426b622151b47ff14c3d56435a214
JinKris <vain65@naver.com>
2022-06-10 20:56:52 +0900

    [feat] add fullpage js to landing page

--- front/componnents/landing/FirstPage.tsx
+++ front/componnents/landing/FirstPage.tsx
@@ -3,7 +3,14 @@ const FirstPage: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.video}>
-        <video autoPlay muted loop data-keepplaying>
+        <video
+          autoPlay
+          muted
+          loop
+          data-keepplaying
+          width={'100%'}
+          height={'80%'}
+        >
           <source src="/assets/forest.mp4" type="video/mp4"></source>
         </video>
       </div>
