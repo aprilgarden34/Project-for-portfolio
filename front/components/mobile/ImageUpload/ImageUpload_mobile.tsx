ae4cc9ccd885465ba90f670d6d8455c0fcdb23ae
Nojongchan <aprilgarden34@gmail.com>
2022-06-14 07:28:13 +0900

    Book, Profile, Upload 컴퍼넌트 최신 업데이트

--- front/components/mobile/ImageUpload/ImageUpload_mobile.tsx
+++ front/components/mobile/ImageUpload/ImageUpload_mobile.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { useState } from 'react';
-=======
 import { useEffect, useRef, useState } from 'react';
->>>>>>> origin/front_feature_NJC
 import Image from 'next/image';
 import styles from './ImageUpload_mobile.module.css';
 import TopNavi from '../TopNavi/TopNavi';
@@ -12,10 +8,6 @@ import { Button, Modal } from 'antd';
 
 const ImageUpload_mobile: React.FC = () => {
   // const [visible, setVisible] = useState(false);
-<<<<<<< HEAD
-  return (
-    <>
-=======
   const [isSnapped, SetisSnapped] = useState(false);
 
   // Preview Photo Logic
@@ -47,7 +39,6 @@ const ImageUpload_mobile: React.FC = () => {
 
   return (
     <div>
->>>>>>> origin/front_feature_NJC
       <div className={styles.container}>
         <TopNavi />
         <div className={styles.middleContainer}>
@@ -62,16 +53,6 @@ const ImageUpload_mobile: React.FC = () => {
                 onClick={() => setVisible(true)}
                 children="촬영 가이드"
               /> */}
-<<<<<<< HEAD
-              <div className={styles.imagePreview}>
-                <Image
-                  src="/images/flora_header_image.jpg"
-                  layout="fill"
-                  objectFit="cover"
-                  draggable={false}
-                />
-              </div>
-=======
 
               <div className={styles.imagePreview}>
                 {isSnapped ? (
@@ -109,15 +90,11 @@ const ImageUpload_mobile: React.FC = () => {
                 )}
               </div>
 
->>>>>>> origin/front_feature_NJC
               {/* <Button className={styles.imageUploadButton} children="+" /> */}
               <Button
                 className={styles.imageClassificationButton}
                 children="판별"
-<<<<<<< HEAD
-=======
                 onClick={snapPhoto}
->>>>>>> origin/front_feature_NJC
               />
             </div>
           </div>
@@ -125,11 +102,7 @@ const ImageUpload_mobile: React.FC = () => {
         <BottomNavi />
       </div>
       {/* <GuideModal_mobile visible={visible} /> */}
-<<<<<<< HEAD
-    </>
-=======
     </div>
->>>>>>> origin/front_feature_NJC
   );
 };
 
