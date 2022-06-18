cfc32c0b13dec2298b12626f7f9862c50eb59871
Nojongchan <aprilgarden34@gmail.com>
2022-06-19 03:31:50 +0900

    [build] 컴포넌트들 위치 수정

--- front/components/desktop/TopHeader/TopHeaderTitle/TopHeaderTitle.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './TopHeaderTitle.module.scss';
-
-const TopHeaderTitle: React.FC = () => {
-  return <h1 className={styles.TopHeaderTitle}>SIMMANI</h1>;
-};
-
-export default TopHeaderTitle;
