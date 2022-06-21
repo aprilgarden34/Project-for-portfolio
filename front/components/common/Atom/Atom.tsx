64f1e2a6300a34e8cbd8fb93d85b17f995569a1c
Nojongchan <aprilgarden34@gmail.com>
2022-06-22 03:43:32 +0900

    [feat] recoil atom 세팅

---
+++ front/components/common/Atom/Atom.tsx
@@ -0,0 +1,11 @@
+import { atom } from 'recoil';
+
+export const searchQuery = atom({
+  key: 'searchQuery',
+  default: '',
+});
+
+export const searchOption = atom({
+  key: 'searchOption',
+  default: 'all',
+});
